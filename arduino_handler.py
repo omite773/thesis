593f5b16b6a1506cba3168635671acae42597ef0
Omid Tern <omite773@student.liu.se>
2019-01-15 09:29:48 +0100

    started working on a log file and the a file which handles communication with arduino which will gather all parameters

---
+++ arduino_handler.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+class Arduino:
+    def __init__(self, arduino_addr):
+        self.addr = arduino_addr
+        self.conn_handle = pi.i2c_open(1, self addr)
+
+    def get_parameters(self):
+        arr = 
+
+    def close(self):
+        pi.i2c_close(self, conn_handle)
+
+
+    
--- data_logger.py
+++ data_logger.py
@@ -1,1 +1,37 @@
-asdlfasdjfnsjdf
+import os
+import sys
+import time
+from datetime import datetime
+import math
+import subprocess
+
+import arduino_handler
+
+
+
+########### Function which handles the opening and saving of data #############
+def append_log():
+    if os.path.isdir("/home/pi/thesis/data_log/"):
+
+        try:
+            # Opens the log file
+            file = open("home/pi/thesis/data_log/data_log.csv", "a")
+        except IOError as e:
+            # Some error logging for good measure
+            print("There were trouble opening the log file")
+            return 2
+
+        # If log file is empty fill the first line with parameter names and units
+        if os.stat("home/pi/thesis/data_log/data_log.csv").st_size == 0:
+            file.write('Date, Time, Temperature [C]\n')
+
+        # Write the measured parameter values into the log file
+        file.write(datetime.now().strtime('%Y-%m-%d') +"," + datetime.now().strtime('%H:%M') + "," + str(temperature) + "," + "\n")
+
+     else:
+         # Error which is shown if log file could not be found i.e. directory not found
+         print("Log dir not present")
+
+
+         
+            
