57e71a1510c13973e508b7d6a8385b2bf14d733c
Omid Tern <omite773@student.liu.se>
2019-01-30 12:46:42 +0100

    added def get_sensor_values()

--- I2C_com/i2c_master.py
+++ I2C_com/i2c_master.py
@@ -4,7 +4,7 @@ import struct
 address = 0x08
 
 # Give the I2C device time to settle
-sleep(2)
+#sleep(2)
 def get_data():
     try:
         with SMBusWrapper(1) as bus:
@@ -18,15 +18,16 @@ def get_float(data, index):
     bytes = bytearray(data[4*index:(index+1)*4])
     return struct.unpack('f', bytes)[0]
 
-data = get_data()
-if(data != -1):
-    temp = round(get_float(data,0),2)
-    c = round(get_float(data,1),2)
-    helo = round(get_float(data,2),2)
-    print(temp)
-    print(c)
-    print(helo)
-    
+def get_sensor_values():
+    result = []
+    data = get_data()
+    if(data != -1):
+        temp = round(get_float(data,0),2)
+        c = round(get_float(data,1),2)
+        helo = round(get_float(data,2),2)
+        result = [temp, c, helo]
+        print(result)
+    return result
 """    
 
 try:
